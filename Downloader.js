fd96dc82a5d5484c9dc4ab62f94f3f5074dd3c37
NCJ <iamncj@outlook.com>
2020-04-16 16:49:11 +0800

    Added Video Download && Fixed Stability

--- Downloader.js
+++ Downloader.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ZJU Tronclass Downloader
 // @namespace    ncj.wiki
-// @version      1.1.1
+// @version      2.0
 // @license      MIT
 // @description  Download Tronclass Courseware
 // @author       NCJ
@@ -10,10 +10,19 @@
 // @require      https://cdn.bootcss.com/jquery/3.5.0/jquery.min.js
 // ==/UserScript==
 
-$(document).ready(function() {
-    $('#file-previewer-with-note > div > div > div.header.clearfix').append('<input type="button" value="Download" id="CP">')
-    $("#CP").css("position", "relative").css("left", 10);
-    $('#CP').click(function(){
-        window.open(decodeURIComponent(document.getElementById('pdf-viewer').src.split("?file=")[1]));
-    });
+$(document).bind('DOMSubtreeModified', function() {
+    if ($('#file-previewer-with-note > div > div > div.header.clearfix').length && $('#Tronclass_Downloader').length == 0) {
+        $('#file-previewer-with-note > div > div > div.header.clearfix').append('<input type="button" value="Download" id="Tronclass_Downloader">')
+        $("#CP").css("position", "relative").css("left", 10);
+        $('#CP').click(function(){
+            window.open(decodeURIComponent(document.getElementById('pdf-viewer').src.split("?file=")[1]));
+        });
+    }
+
+    if ($('video').length && $('video')[0].children.length == 3 && $('#Tronclass_Downloader').length == 0) {
+        var v=$('video')[0];
+        for (var i of v.children) {
+            $(v.parentNode.parentNode).prepend(`<a href="${i.getAttribute('src')}" id="Tronclass_Downloader">\t${i.getAttribute('label')}\t</a>`);
+        }
+    }
 });
